08a2aa2eac81b1a136898678d5101b01d1c3d450
Thorvald <tvanbeek@roc-dev.com>
2020-01-16 10:34:56 +0100

    Update exercise.sql

--- exercise.sql
+++ exercise.sql
@@ -1,10 +1,8 @@
 CREATE DATABASE Sterrenstelsel ;
 USE Sterrenstelsel;
--- Create a new table called 'Planeten' in schema 'SchemaName'
--- Drop the table if it already exists
+
 CREATE TABLE Sterrenstelsel.Planeten
 (
     Planeten INT NOT NULL PRIMARY KEY, -- primary key column
     Naam NVARCHAR(50) NOT NULL
-    -- specify more columns here
 );
