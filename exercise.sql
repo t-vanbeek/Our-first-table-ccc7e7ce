c4fce8bcc2c2ada2a3747b52ef5e4232e87c423b
Thorvald van Beek <tvanbeek@roc-dev.com>
2020-01-16 10:07:03 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,1 +1,9 @@
-CREATE DATABASE Sterrenstelsel
+CREATE DATABASE Sterrenstelsel ;
+-- Create a new table called 'Planeten' in schema 'SchemaName'
+-- Drop the table if it already exists
+CREATE TABLE Sterrenstelsel.Planeten
+(
+    Planeten INT NOT NULL PRIMARY KEY, -- primary key column
+    Naam NVARCHAR(50) NOT NULL
+    -- specify more columns here
+);
